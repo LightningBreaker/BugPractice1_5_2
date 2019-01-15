321ea8150c86e0363365ec1c37589f0ddc787e17
641507503 <641507503@qq.com>
2019-01-15 12:15:09 +0800

    12：14  1—15 zmy

--- BugPractice1_4/Global_Database.cs
+++ BugPractice1_4/Global_Database.cs
@@ -11,7 +11,7 @@ namespace BugPractice1_4
     class Global_Database
     {
         public static string Conn = "Database='practice_in_school';" +
-            "Data Source='';User Id='root';" +
+            "Data Source='192.168.43.116';User Id='root';" +
             "Password='TheFirstDon';charset='utf8';pooling=true";
         
         public static Dictionary<string,string> SearchTesterInfo()
